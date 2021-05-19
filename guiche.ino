94e75e3c87ad8e96ef47021f21e0b6e2f790d7a6
joao2325 <47531461+joao2325@users.noreply.github.com>
2021-05-19 22:23:09 +0100

    Add files via upload

--- guiche.ino
+++ guiche.ino
@@ -6,28 +6,38 @@ LiquidCrystal lcd(7, 8, 9, 10, 11 , 12);
 int inputPin = 5;
 
 int currentState;
+int num = 0;
  
 void setup() { 
 
   Serial.begin(9600);
 
-  pinMode(2, INPUT_PULLUP);
+  pinMode(2, INPUT);
 
   pinMode(inputPin, OUTPUT);
-  digitalWrite(inputPin,HIGH);
+  //digitalWrite(inputPin,HIGH);
   lcd.begin(16, 2);
   lcd.setCursor(0,0);
   lcd.write("Fila de espera:");
   lcd.setCursor(0,1);
-  lcd.write("25");
+  String s = String(num);
+  lcd.print(s);
 }
  
 void loop() { 
   currentState = digitalRead(2);
-  digitalWrite(inputPin,HIGH);
+  
 
-  if(currentState == LOW){
-    Serial.println("The button is pressed");
+  if(currentState == HIGH){
+    digitalWrite(inputPin,LOW);
+  }
+  else
+  {
+    digitalWrite(inputPin,HIGH);
+    num++;
+    lcd.setCursor(0,1);
+    String s = String(num);
+    lcd.print(s);
   }
   Serial.println(currentState);
   delay(1000);
