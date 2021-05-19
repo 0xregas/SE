18af65a3953002882ebf9a8c871872aeb50af8b9
joao2325 <47531461+joao2325@users.noreply.github.com>
2021-05-19 19:01:20 +0100

    Add files via upload

--- guiche.ino
+++ guiche.ino
@@ -1,18 +1,20 @@
-  
+#include <MD_KeySwitch.h>
 #include <LiquidCrystal.h>
  
 LiquidCrystal lcd(7, 8, 9, 10, 11 , 12);
 
-const int inputPin = 2;
+int inputPin = 5;
 
 int currentState;
  
 void setup() { 
 
-  Serial.begin(9600 );
+  Serial.begin(9600);
 
-  pinMode(inputPin, INPUT);
-  
+  pinMode(2, INPUT_PULLUP);
+
+  pinMode(inputPin, OUTPUT);
+  digitalWrite(inputPin,HIGH);
   lcd.begin(16, 2);
   lcd.setCursor(0,0);
   lcd.write("Fila de espera:");
@@ -21,11 +23,12 @@ void setup() {
 }
  
 void loop() { 
-  currentState = digitalRead(inputPin);
+  currentState = digitalRead(2);
+  digitalWrite(inputPin,HIGH);
 
-  if(currentState == HIGH){
+  if(currentState == LOW){
     Serial.println("The button is pressed");
   }
-
   Serial.println(currentState);
+  delay(1000);
 }
