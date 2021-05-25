265e9001a5d4d6391c3a1525b6ab829d2e3d020a
JMiguel <joao.miguel@glintt.com>
2021-05-25 22:53:53 +0100

    Added action bar back button, so we can go back to the selection menu.

--- app/src/main/java/com/example/giche/MainActivity.kt
+++ app/src/main/java/com/example/giche/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.giche
 
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
@@ -15,38 +16,52 @@ import kotlin.concurrent.timerTask
 
 var currId = 0
 var Ip= 0
+var senha = "0"
+var tsenha = 0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getIp()
+        changeToMain()
+    }
+
+    fun changeToMain(){
         setContentView(R.layout.activity_main)
     }
 
     fun changeTalho(view: View) {
         currId = 1
-        getCurrent()
-        getAverage()
-        getNext()
-        Timer().schedule(timerTask { getNext()
-                                    getCurrent()
-                                    getAverage()}
-                                    ,10000, 10000)
-
-        setContentView(R.layout.option_picked_main)
+       changeToOptionPicked()
     }
 
     fun changePeixaria(view: View) {
         currId = 2
+        changeToOptionPicked()
+    }
+
+    fun changeToOptionPicked(){
         getCurrent()
         getAverage()
         getNext()
         Timer().schedule(timerTask { getNext()
-                                    getCurrent()
-                                    getAverage()}
-                                    ,10000, 10000)
+            getCurrent()
+            getAverage()}
+            ,10000, 10000)
+        setContentView(R.layout.option_picked_main)
+        val actionBar = getSupportActionBar()
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+    }
 
-    setContentView(R.layout.option_picked_main)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home ->{
+                changeToMain()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun getAverage() {
@@ -127,14 +142,19 @@ class MainActivity : AppCompatActivity() {
         val url = "http://192.168.1."+Ip+":8080/getNew?id=" + currId
         val stringRequest = StringRequest(Request.Method.GET, url,
             Response.Listener<String> { response ->
-                buttonSenha.visibility = View.INVISIBLE
-                currSenha.text = response.toString()
-                headerSenha.visibility = View.VISIBLE
-                currSenha.visibility = View.VISIBLE
+                senha = response.toString()
+                tsenha = currId
+                newSenhaPress()
             },
             Response.ErrorListener {
             })
         requestQueue.add(stringRequest)
+    }
+
+    fun newSenhaPress(){
+        buttonSenha.visibility = View.INVISIBLE
+        headerSenha.visibility = View.VISIBLE
+        currSenha.visibility = View.VISIBLE
     }
 
 }
